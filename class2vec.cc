9e4b7c0029254c25e68cf99fafef80a58d5fc059
gnoynait <gnoynait@gmail.com>
2014-07-02 17:50:40 +0800

    2

--- class2vec.cc
+++ class2vec.cc
@@ -2,14 +2,18 @@
 #include<fstream>
 #include<cmath>
 #include<vector>
+#include<unordered_map>
 using namespace std;
-
+typedef long long code_t;
+#define MAX_CODE_LEN 60
+#define MAX_RECORD_WORDS 60
 struct Record {
     string code;
     vecotr<int> nodes;
     vector<int> words;
 };
-
+unordered_map<code_t, int> code_index;
+int latest_code_index = 0;
 int vec_size = 100;
 int vocab_size;
 int class_num;
@@ -43,28 +47,77 @@ void init_net() {
         }
     }
 }
+int read_record(FILE *fin, int *codes, int *nodes, 
+    int *words, int &len_codes, int &len_words) {
 
-void train_model() {
-    vector<int> codes;
-    vector<int> nodes;
-    vector<int> words;
+    int a = 0;
+    int ch = 0, pre_ch;
+    code_t code = 0;
+    int read_codes = 1;
+    len_codes = 0;
+    len_words = 0;
+    if (feof(fin)) {
+        return 0;
+    }
+    while (!feof(fin)) {
+        pre_ch = ch;
+        ch = fgetc(fin);
+        if (ch == 13) {
+            continue; // carriage return
+        }
+        if (ch == '\n') {
+            break;
+        }
+        if ((ch == ' ') || (ch == '\t')) {
+            if (read_codes == 1) {
+                read_codes = 0;
+            } else {
+                if (pre_ch >= '0' && pre_ch <= '9') {
+                    ++len_words;
+                }
+            }
+        } else {
+            if (read_codes == 0) {
+                assert(ch == '0' || ch == '1');
+                codes[len_codes] = ch == '0' ? 0 : 1;
+                if (ch == '0') {
+                    code = code << 1;
+                } else {
+                    code = code << 1 & 1;
+                }
+                if (code_index.count(code) == 0) {
+                    code_index[code] = latest_code_index++;
+                }
+                nodes[len_codes] = code_index[code];
+                ++len_codes;
+            } else {
+                assert(ch >= '0' && ch <= '9');
+                words[len_words] = words[len_words] * 10 + ch - '0';
+            }
+        }
+    }
+    return 1;
+}
+void train_model(FILE *train_file) {
+    int codes[MAX_CODE_LEN];
+    int nodes[MAX_CODE_LEN];
+    int words[MAX_RECORD_WORDS];
+    int len_codes, len_words;
     int record_count = 0;
     float alpha;
-    while (1) {
-        // read nodes
-        // read words;
+    while (read_record(train_file, codes, nodes, len_codes, len_words )) {
         alpha = starting_alpha * (1 - record_count / (float)100000);
         if (alpha < starting_alpha * 0.0001) alpha = starting_alpha * 0.0001;
         for (int i = 0; i < vec_size; ++i) {
             neu[i] = 0;
             eneu[i] = 0;
         }
-        for (int i = 0; i < words.size(); ++i) {
+        for (int i = 0; i < len_words; ++i) {
             for (int j = 0; j < vec_size; ++j) {
                 neu[j] += syn0[words[i] * vec_size + j];
             }
         }
-        for (int i = 0; i < nodes.size(); ++i) {
+        for (int i = 0; i < len_nodes; ++i) {
             float f = 0;
             float g;
             for (int j = 0; j < vec_size; ++j) {
@@ -79,7 +132,7 @@ void train_model() {
                 syn1[nodes[i] * vec_size + c] += g * neu[c];
             }
         }
-        for (int i = 0; i < words.size(); i++) {
+        for (int i = 0; i < len_words; i++) {
             for (int c = 0; c < vec_size; c++) {
                 syn0[c + words[i] * vec_size] += enue[c];
             }
