c31addbcec1fff5bf56524a9c2fe2ceb83467e90
gnoynait <gnoynait@gmail.com>
2014-07-03 17:53:25 +0800

    3

--- class2vec.cc
+++ class2vec.cc
@@ -7,20 +7,70 @@ using namespace std;
 typedef long long code_t;
 #define MAX_CODE_LEN 60
 #define MAX_RECORD_WORDS 60
-struct Record {
+#define MAX_WROD_LEN 60
+/*struct Record {
     string code;
     vecotr<int> nodes;
     vector<int> words;
 };
+*/
 unordered_map<code_t, int> code_index;
+unordered_map<string, int> word_count; // count each number for filter words
+unordered_map<string, int> vocab_index;
 int latest_code_index = 0;
 int vec_size = 100;
 int vocab_size;
 int class_num;
+int min_word_count = 20;
 float *syn0, *syn1, *neu, *eneu;
 float *expTable;
 clock_t start;
 float starting_alpha = 0.025;
+// read a word from fin
+// return: 0 if '\n'
+//         1 if read a word
+//        -1 if end of file
+int read_word(FILE *fin, char *buffer) {
+    if (feof(fin)) return -1;
+    int len = 0;
+    while (!feof(fin)) {
+        ch = fgetc(fin);
+        if (ch == '\n') {
+            if (len == 0) return 0;
+            break;
+        } else if (ch == ' ' || ch == '\t') {
+            if (len == 0) continue;
+            break;
+        } else {
+            buffer[len++] = ch;
+        }
+    }
+    buffer[len] = '\0';
+    return 1;
+}
+void learn_vocab(FILE *train_file) {
+    int index = 0;
+    fseek(train_file, 0, SEEK_SET);
+    char word[MAX_WORD_LEN];
+    int status;
+    int new_record = 1;
+    while((status = read_word(train_file, word)) != -1) {
+        // TODO
+        if (status == 0) {
+            new_record = 1;
+        } else {
+            word_count[word]++;
+            new_record = 0;
+        }
+    }
+    for (auto it = word_count.begin(); it != word_count.end(); ++it) {
+        if (it->second > min_word_count) {
+            vocab_index[it->first] = index;
+            ++index;
+        }
+    }
+}
+
 void init_net() {
 
     syn0 = new float[vec_size * vocab_size];
@@ -138,4 +188,37 @@ void train_model(FILE *train_file) {
             }
         }
     }
+}
+
+void save_model(FILE *vocab_vec_file, FILE *class_vec_file) {
+    for (int v = 0; v < vocab_size; ++v) {
+        for (int i = 0; i < vec_size; ++i) {
+            char sep = i == vec - 1 ? '\n' : ' ';
+            printf ("%f", syn0[v * vec_size + i]);
+            fprintf(vocab_vec_file, "%f%c", sep);
+        }
+    }
+    for (unordered_map<code_t, int>::iterator it = code_index.begin(); it != code_index.end(); ++it) {
+        code_t code = it->code;
+        int index = it->index;
+        // TODO not easy to implement
+    }
+}
+int main () {
+    FILE *train_file;
+    FILE *vocab_vec_file;
+    FILE *class_vec_file;
+    trian_file = fopen("train.dat", "r");
+    vocab_vec_file = fopen("vocab_vec.dat", "w");
+    class_vec_file = fopen("class_vec.dat", "w");
+    if (!(train_file && vocab_vec_file && class_vec_file)) {
+        printf ("Open file error\n");
+        exit(1);
+    }
+    train_model(train_file);
+    save_model(vocab_vec_file, class_vec_file);
+    fclose(train_file);
+    fclose(vocab_vec_file);
+    fclose(class_vec_file);
+    return 0;
 }
