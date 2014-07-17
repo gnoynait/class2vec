e8cf656039162114f840c982422731f45f1bed72
gnoynait <gnoynait@gmail.com>
2014-07-17 08:25:39 -0700

    class2vec_flat can't run

--- class2vec_flat.cc
+++ class2vec_flat.cc
@@ -1,21 +1,45 @@
 #include<cmath>
+#include<cassert>
+#include<map>
+#include<ctime>
+#include<cstdlib>
+#include<cstring>
 #include<string>
 #include<iostream>
+#include<fstream>
 #include<vector>
-int vec_size;
+using namespace std;
+
+// dimension of word and class vector
+int vec_size = 20;
+// size of vocabulary
 int word_num;
+// number of class
 int class_num;
-int max_record_words;
-int min_record_words;
+// maximum words in a record, remaining words will be ignored
+int max_record_words = 100;
+// minimum words in a record, record has fewer words will be ignored
+int min_record_words = 3;
+// minimum apperance of a word, words appeared fewer than this will be ignored
 int min_word_count = 20;
+// how many times to go through th train file
+int max_round = 2;
+// initial update rate
+float start_alpha = 0.025;
+
 float **word_vec;
 float **class_vec;
+
+// index in word_vec of a word
+map<string, int> word_table;
+// index in class_vec of a class
+map<string, int> class_table;
+
 ifstream train_file;
 ofstream word_vec_file;
 ofstream class_vec_file;
-map<string, int> word_table;
-map<string, int> class_table;
 
+// exp(vec1 * vec2)
 float expprod(float *vec1, float *vec2) {
     float p = 0;
     for (int i = 0; i < vec_size; i++) {
@@ -24,46 +48,72 @@ float expprod(float *vec1, float *vec2) {
     return exp(p);
 }
 
-void init_net() {
-    word_vec = new float *[word_num];
-    for (int i = 0; i < word_num; i++) {
-        word_vec[i] = new float[vec_size];
-    }
-    class_vec = new float *[class_num];
-    for (int i = 0; i < class_num; i++) {
-        class_vec[i] = new float[vec_size];
-    }
-}
+// set vec = 0
 void clear_vec(float *vec) {
     for (int i = 0; i < vec_size; i++) {
         vec[i] = 0;
     }
 }
 
-void update_vec(float *vec1, float *vec2, float *rate) {
+// set vec1 = vec1 + rate * vec2
+void update(float *vec1, float *vec2, float rate) {
     for (int i = 0; i < vec_size; i++) {
         vec1[i] += rate * vec2[i];
     }
 }
 
+// allocate memory for word_vec and class_vec and give 
+// them random initial value
+void init_net() {
+    word_vec = new float *[word_num];
+    class_vec = new float *[class_num];
+    if (!(word_vec && class_vec)) {
+        cerr << "MEMORY ALLOCATION ERROR" << endl;
+        exit(-1);
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < word_num; i++) {
+        word_vec[i] = new float[vec_size];
+        assert(word_vec[i]);
+        for (int j = 0; j < vec_size; j++) {
+            word_vec[i][j] = 2.0 * ((float) rand() / RAND_MAX - 0.5);
+        }
+    }
+    for (int i = 0; i < class_num; i++) {
+        class_vec[i] = new float[vec_size];
+        assert(class_vec[i]);
+        for (int j = 0; j < vec_size; j++) {
+            class_vec[i][j] = 2.0 * ((float) rand() / RAND_MAX - 0.5);
+        }
+    }
+}
+
+// read a record, which is a list of string, from train_file
+// return 1 if success else 0
 int read_record(vector<string> &record) {
     string line;
     record.clear();
     if(!getline(train_file, line)) return 0;
+
     int begin = 0, end = 0;
     while(begin < line.length()) {
-        while (begin < line.length() && line[begin] == '\t' || line[begin] == ' ') begin++;
+        while (begin < line.length() && (line[begin] == '\t' 
+            || line[begin] == ' ')) begin++;
         if (begin == line.length()) break;
+
         end = begin + 1;
-        while(end < line.length() && end != '\t' && end != ' ') {
+        while(end < line.length() && line[end] != '\t' && line[end] != ' ') {
             end++;
         }
         record.push_back(line.substr(begin, end - begin));
+
         begin = end + 1;
     }
     return 1;
 }
 
+// learn vocabulary and classes from the train file
 void learn_vocab() {
     train_file.seekg(0, train_file.beg);
     vector<string> record;
@@ -72,50 +122,67 @@ void learn_vocab() {
     map<string, int> word_count;
     while (read_record(record)) {
         if (record.size() == 0) continue;
-        if (class_table.cound(record[0]) == 0) {
+        if (class_table.count(record[0]) == 0) {
             class_table[record[0]] = class_num++;
         }
         for (int i = 1; i < record.size(); i++) {
             word_count[record[i]]++;
         }
     }
-    for (map<string, int>::iterator it = word_count.begin(); it != word_count.end(); it++) {
+    for (map<string, int>::iterator it = word_count.begin();
+        it != word_count.end(); it++) {
         if (it->second < min_word_count) continue;
         word_table[it->first] = word_num++;
     }
 }
 
+// get a instance form train file, words are transformed to its index
+// return 1 if success else 0
 int get_instance(int &class_id, vector<int> &words) {
     vector<string> record;
     words.clear();
     int nword = 0;
-    if (read_record(record)) return 0;
+    if (read_record(record) == 0) return 0;
     class_id = word_table[record[0]];
     for (int i = 1; i < record.size() && nword < max_record_words; i++) {
         if (word_table.count(record[i]) == 0) continue;
         nword++;
-        words.append(word_table[record[i]]);
+        words.push_back(word_table[record[i]]);
     }
     return 1;
 }
 
+// train the model max_round times
 void train() {
     int round = 0;
-    vector<vector<float> > expprod_table(max_record_words, vector<float>(class_num, 0));
+    int record_count = 0;
+    int round_records = 0;
+    // expprod_table[w][c] = exp(word_vec[w] * class_vec[c])
+    vector<vector<float> > expprod_table(max_record_words,
+        vector<float>(class_num, 0));
+    // update buffer
     float *vec = new float[vec_size];
+    assert(vec);
+    train_file.seekg(0, train_file.beg);
     while (round < max_round) {
         int class_id;
         vector<int> words;
-        train_file.seekg(0, train_file.beg);
 
         if (get_instance(class_id, words) == 0) {
             round++;
+            cerr << round << ' ' << round_records << endl;
+            round_records = 0;
+            train_file.seekg(0, train_file.beg);
             continue;
         }
+        round_records++;
 
         if (words.size() < min_record_words) continue;
 
-        float alpha = start_alpha * (1 - round / 100000);
+        float alpha = start_alpha * (1 - (float)record_count / 100000);
+        if (record_count < 100000) {
+            record_count++;
+        }
         alpha = alpha < 0.0001 ? 0.0001 : alpha;
 
         float A = 0;
@@ -144,40 +211,61 @@ void train() {
         for (int c = 0; c < class_num; c++) {
             clear_vec(vec);
             if (c == class_id) {
-                for (w = 0; w < words.size(); w++) {
+                for (int w = 0; w < words.size(); w++) {
                     update(vec, word_vec[words[w]], A * expprod_table[w][c]); 
                 }
             }
-            for (w = 0; w < words.size(); w++) {
+            for (int w = 0; w < words.size(); w++) {
                 update(vec, word_vec[words[w]], - s * expprod_table[w][c]);
             }
             update(class_vec[c], vec, alpha / A / A);
         }
     }
 }
+
+// save word_vec and class_vec into file
 void save_model() {
-    for (map<string, int>::iterator it = word_table.begin(); it != word_table.end(); it++) {
+    word_vec_file << word_num << '\t' << vec_size << endl;
+    for (map<string, int>::iterator it = word_table.begin();
+        it != word_table.end(); it++) {
         word_vec_file << it->first;
         for (int i = 0; i < vec_size; i++) {
-            word_vec_file << ' ' << word_vec[it->second][i];
+            word_vec_file << '\t' << word_vec[it->second][i];
         }
         word_vec_file << endl;
     }
-    for (map<string, in>::iterator it = class_table.begin(); it != class_table.end(); it++) {
+
+    class_vec_file << class_num << endl;
+    for (map<string, int>::iterator it = class_table.begin();
+        it != class_table.end(); it++) {
         class_vec_file << it->first;
         for (int i = 0; i < vec_size; i++) {
-            class_vec_file << ' ' << class_vec[it->second][i];
+            class_vec_file << '\t' << class_vec[it->second][i];
         }
         class_vec_file << endl;
     }
 }
 
 int main (int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-s") == 0) {
+            vec_size = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-m") == 0) {
+            min_word_count = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-r") == 0) {
+            max_round = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-n") == 0) {
+            min_record_words = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-w") == 0) {
+            max_record_words = atoi(argv[i + 1]);
+        }
+    }
+
     train_file.open("train.dat");
     word_vec_file.open("word_vec.dat", ofstream::out);
     class_vec_file.open("class_vec.dat", ofstream::out);
-    
-    if (!(train_file.is_open() && word_vec_file.is_open() && class_vec_file.is_open())) {
+    if (!(train_file.is_open() && word_vec_file.is_open()
+        && class_vec_file.is_open())) {
         cerr << "CAN'T OPEN FILE." << endl;
         return -1;
     }
--- predict_flat.cc
+++ predict_flat.cc
@@ -1,22 +1,36 @@
+#include<cstdlib>
 #include<cmath>
+#include<cassert>
+#include<cstring>
 #include<string>
 #include<iostream>
+#include<fstream>
 #include<vector>
+#include<map>
+using namespace std;
+
+// dimension of vector
 int vec_size;
+// vocabulary size
 int word_num;
+// number of class
 int class_num;
-int max_record_words;
-int min_record_words;
+int max_record_words = 10;
+int min_record_words = 3;
 int min_word_count = 20;
+
 float **word_vec;
 float **class_vec;
-ifstream word_vec_file;
-ifstream class_vec_file;
-ofstream target_file;
-ofstream result_file;
+
 map<string, int> word_table;
 vector<string> class_table;
 
+ifstream word_vec_file;
+ifstream class_vec_file;
+ifstream target_file;
+ofstream result_file;
+
+// exp(vec1 * vec2)
 float expprod(float *vec1, float *vec2) {
     float p = 0;
     for (int i = 0; i < vec_size; i++) {
@@ -25,74 +39,67 @@ float expprod(float *vec1, float *vec2) {
     return exp(p);
 }
 
+// allocate memory for word_vec and class_vec
 void init_net() {
     word_vec = new float *[word_num];
+    class_vec = new float *[class_num];
+    if (!(word_vec && class_vec)) {
+        cerr << "MEMOERY ALLOCATION ERROR" << endl;
+        exit(-1);
+    }
     for (int i = 0; i < word_num; i++) {
         word_vec[i] = new float[vec_size];
+        assert(word_vec[i]);
     }
-    class_vec = new float *[class_num];
     for (int i = 0; i < class_num; i++) {
         class_vec[i] = new float[vec_size];
+        assert(class_vec[i]);
     }
 }
 
 void load_model() {
     string word, class_name;
-    int w = 0;
-    int c = 0;
+
     word_vec_file >> word_num >> vec_size;
     class_vec_file >> class_num;
-    class_table.reserve(class_num);
+
     init_net();
-    for (int i = 0; i < word_num; i++) {
+    for (int w = 0; w < word_num; w++) {
         word_vec_file >> word;
         word_table[word] = w;
         for (int j = 0; j < vec_size; j++) {
             word_vec_file >> word_vec[w][j];
         }
-        w++;
     }
-    for (int i = 0; i < class_num; i++) {
-        word_vec_file >> class_name;
-        class_table[c] = class_name;
+
+    for (int c = 0; c < class_num; c++) {
+        class_vec_file >> class_name;
+        class_table.push_back(class_name);
         for (int j = 0; j < vec_size; j++) {
             class_vec_file >> class_vec[c][j];
         }
-        c++;
     }
 }
+
 int read_record(vector<string> &record) {
     string line;
     record.clear();
-    if(!getline(train_file, line)) return 0;
-    int begin = 0, end = 0;
+    if(!getline(target_file, line)) return 0;
+
+    int begin = 0, end;
     while(begin < line.length()) {
-        while (begin < line.length() && line[begin] == '\t' || line[begin] == ' ') begin++;
+        while (begin < line.length() && (line[begin] == '\t'
+            || line[begin] == ' ')) begin++;
         if (begin == line.length()) break;
+ 
         end = begin + 1;
-        while(end < line.length() && end != '\t' && end != ' ') {
+        while(end < line.length() && line[end] != '\t' && line[end] != ' ') {
             end++;
         }
         record.push_back(line.substr(begin, end - begin));
         begin = end + 1;
     }
     return 1;
-}
-
-int predict(vector<int> &words) {
-    float maxv = 0;
-    int result = -1;
-    for (int c = 0; c < class_num; c++) {
-        float v = 0;
-        for (int w = 0; w < words.size(); w++) {
-            v += expprod(word_vec[w], class_vec[c]);
-        }
-        if (v > maxv) {
-            result = c;
-            maxv = v;
-        }
-    }
-    return result;
 }
 
 int get_instance(vector<int> &words) {
@@ -103,15 +110,31 @@ int get_instance(vector<int> &words) {
     for (int i = 0; i < record.size() && nword < max_record_words; i++) {
         if (word_table.count(record[i]) == 0) continue;
         nword++;
-        words.append(word_table[record[i]]);
+        words.push_back(word_table[record[i]]);
     }
     return 1;
+}
+
+int predict(vector<int> &words) {
+    float maxv = 0;
+    int result = 0;
+    for (int c = 0; c < class_num; c++) {
+        float v = 0;
+        for (int w = 0; w < words.size(); w++) {
+            v += expprod(word_vec[words[w]], class_vec[c]);
+        }
+        if (v > maxv) {
+            result = c;
+            maxv = v;
+        }
+    }
+    return result;
 }
 
 void process() {
     vector<int> words;
     while(get_instance(words)) {
-        if (words.length() < min_record_words) {
+        if (words.size() < min_record_words) {
             result_file << "IGNORE" << endl;
             continue;
         }
@@ -119,21 +142,35 @@ void process() {
         result_file << class_table[result] << endl;
     }
 }
+
 int main (int argc, char *argv[]) {
     word_vec_file.open("word_vec.dat");
     class_vec_file.open("class_vec.dat");
     target_file.open("target.dat");
     result_file.open("result.dat", ofstream::out);
-    
-    if (!(target_file.is_open() && result_file.is_open() && word_vec_file.is_open() && class_vec_file.is_open())) {
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-w") == 0) {
+            max_record_words = atoi(argv[i + 1]);
+        }
+        if (strcmp(argv[i], "-n") == 0) {
+            min_record_words = atoi(argv[i + 1]);
+        }
+    }
+
+    if (!(target_file.is_open() && result_file.is_open()
+        && word_vec_file.is_open() && class_vec_file.is_open())) {
         cerr << "CAN'T OPEN FILE." << endl;
         return -1;
     }
+
     load_model();
     process();
+
     word_vec_file.close();
     class_vec_file.close();
     target_file.close();
     result_file.close();
+
     return 0;
 }
